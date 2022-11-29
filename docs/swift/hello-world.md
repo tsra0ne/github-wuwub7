01b9f944964be68be10c8a4239189639b7f01230
tsra0ne <tsravangoud164@gmail.com>
2022-11-29 12:45:50 +0530

    Initial commit

---
+++ docs/swift/hello-world.md
@@ -0,0 +1,57 @@
+---
+sidebar_position: 1
+---
+
+# Swift Hello World
+
+**In this tutorial, you will learn to write a program to print "Hello, World!" in Swift**. s
+
+A Hello World is a simple program that prints `Hello, World!` on the screen. This program is often used to introduce a new programming language to beginners.
+
+Let's explore how to create "Hello, World!" program in Swift.
+
+### Swift "Hello, World!" Program
+
+```swift
+// Swift "Hello, World!" Program
+
+print("Hello, World!")
+```
+
+#### Output
+
+```text
+Hello, World!
+```
+
+:::note
+
+If you want to run this program on your computer, make sure that Swift is properly installed. To learn more about setting up Swift on your computer, visit _Getting Started with Swift_.
+
+:::
+
+### How Swift "Hello, World!" Program Works?
+
+Here are the different sections of the Hello World program in Swift:
+
+#### 1. Swift Comment
+
+```swift
+// Swift "Hello, World!" Program
+```
+
+We use two slashes `//` to write comments in Swift.
+
+Comments are hints that we can add to our program to make our code more understandable. They are meant for people reading the code. To learn more, visit _Swift Comments_.
+
+#### 2. print Statement
+
+```swift
+print("Hello, World!")
+```
+
+The code above displays the text `Hello, World!` on your screen.
+
+Congratulations! You just wrote your first program in Swift.
+
+Next we'll learn about variables, constants, and literals in Swift.
---
+++ docusaurus.config.js
@@ -0,0 +1,137 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'tsra0ne',
+  tagline: 'My Docs',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: 'https://github.com/tsra0ne/tsra0ne/tree/gh-pages',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: 'https://github.com/tsra0ne/tsra0ne/tree/gh-pages',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'tsra0ne',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          { to: '/blog', label: 'Blog', position: 'left' },
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+          {
+            type: 'doc',
+            docId: 'swift/hello-world',
+            position: 'left',
+            label: 'Swift',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/tsra0ne',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/tsra0ne',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} tsra0ne`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+        additionalLanguages: ['swift'],
+      },
+    }),
+};
+
+module.exports = config;
